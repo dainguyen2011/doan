65212230a1592762119eb916bcebc60d5a7e4c6d
daint <daint02@ominext.com>
2020-05-19 17:22:25 +0700

    v1 t3

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -173,6 +173,21 @@
 
 <div id="fb-root"></div>
 <script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v6.0&appId=232587937746885&autoLogAppEvents=1"></script>
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/5ec3b2d06f7d401ccbb7e2c9/default';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+    })();
+</script>
+<!--End of Tawk.to Script-->
+
 </body>
 
 </html>
